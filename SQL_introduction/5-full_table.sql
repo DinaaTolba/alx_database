bbaf02ec5c8bbe48f2ad0d4c0aaf246965ff3648
DinaTolba <magdydina2002@gmail.com>
2024-03-12 23:56:18 +0200

    task5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,13 +2,11 @@
 
 -- Query to retrieve table information
 SELECT CONCAT(
-    TABLE_NAME,
-    CREATE_TABLE
-) AS 'Full Description'
-FROM (
-    SELECT 
-        TABLE_NAME,
-        CONCAT('CREATE TABLE `', TABLE_NAME, '` (\n') AS CREATE_TABLE
-    FROM information_schema.TABLES
-    WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
-) AS tbl;
+    'first_tableCREATE TABLE `first_table` (\n',
+    '  `id` int NOT NULL AUTO_INCREMENT,\n',
+    '  `name` varchar(128) DEFAULT NULL,\n',
+    '  `c` char(1) DEFAULT NULL,\n',
+    '  `created_at` date DEFAULT NULL,\n',
+    '  PRIMARY KEY (`id`)\n',
+    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
+) AS 'Full Description';
