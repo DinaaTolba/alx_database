c1dfe4df2b1a24d0fbbc195df48bf56c783eb777
DinaTolba <magdydina2002@gmail.com>
2024-03-12 23:59:26 +0200

    task9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+-- Script to list all rows of the table 'first_table' from the database 'hbtn_0c_0'
+
+-- Query to select all fields from the table 'first_table'
+SELECT *
+FROM hbtn_0c_0.first_table;
