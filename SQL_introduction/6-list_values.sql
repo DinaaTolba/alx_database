ef3ad1522d1cc18981a8ec287e2d6eb2ff0f8eba
DinaTolba <magdydina2002@gmail.com>
2024-03-13 00:11:02 +0200

    task6

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,5 +1,8 @@
 -- Script to list all rows of the table 'first_table' from the database 'hbtn_0c_0'
 
+-- Use the database
+USE hbtn_0c_0;
+
 -- Query to select all fields from the table 'first_table'
-SELECT *
-FROM hbtn_0c_0.first_table;
+SELECT id, name
+FROM first_table;
