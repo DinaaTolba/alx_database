5f88e2f0cdb2a6b0914ce3433ebdde23da9f08a8
DinaTolba <magdydina2002@gmail.com>
2024-03-13 00:03:39 +0200

    task6

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,6 +1,9 @@
 -- Script to list all rows of the table 'first_table' from the database 'hbtn_0c_0'
 
--- Check if the database exists
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Use the database
 USE hbtn_0c_0;
 
 -- Query to select all fields from the table 'first_table'
