18a5a127e7b79e0b56ba42614be917dd086fb089
DinaTolba <magdydina2002@gmail.com>
2024-03-13 00:07:55 +0200

    task9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,15 @@
--- Script to list all rows of the table 'first_table' from the database 'hbtn_0c_0'
+-- Script to create the table 'second_table' in the database 'hbtn_0c_0' and add multiple rows
 
--- Query to select all fields from the table 'first_table'
-SELECT *
-FROM hbtn_0c_0.first_table;
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert multiple rows into the table
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
