7190cbfc1216cb3ac1812bb7a8d2be4ed7931630
DinaTolba <magdydina2002@gmail.com>
2024-03-13 00:26:39 +0200

    task0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant all privileges to user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
