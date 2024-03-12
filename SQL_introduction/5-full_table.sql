e4caa2846262d0afc8011b64623d7b92481f2049
DinaTolba <magdydina2002@gmail.com>
2024-03-12 23:54:54 +0200

    task5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,14 @@
 -- Script to print the full description of the table 'first_table' from the database 'hbtn_0c_0'
 
 -- Query to retrieve table information
-SELECT CONCAT('Table: ', TABLE_NAME) AS 'Table',
-       CONCAT('Create Table: ', CREATE_TABLE) AS 'Create Table'
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME =
+SELECT CONCAT(
+    TABLE_NAME,
+    CREATE_TABLE
+) AS 'Full Description'
+FROM (
+    SELECT 
+        TABLE_NAME,
+        CONCAT('CREATE TABLE `', TABLE_NAME, '` (\n') AS CREATE_TABLE
+    FROM information_schema.TABLES
+    WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
+) AS tbl;
