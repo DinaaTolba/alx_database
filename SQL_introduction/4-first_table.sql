8ff4c619af2e286e871b32edaec871003f945d23
DinaTolba <magdydina2002@gmail.com>
2024-03-12 23:52:07 +0200

    task4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script to create the table 'first_table' in the current database
+
+-- Check if the table exists, and if not, create it
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
