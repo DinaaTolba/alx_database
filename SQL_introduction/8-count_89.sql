65edc422f71ad017c62bc6449a5b0af8090a3823
DinaTolba <magdydina2002@gmail.com>
2024-03-13 00:06:27 +0200

    task8

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- Script to count the number of records with id = 89 in the table 'first_table' of the database 'hbtn_0c_0'
+
+-- Query to count the records
+SELECT COUNT(*) AS count_89
+FROM first_table
+WHERE id = 89;
