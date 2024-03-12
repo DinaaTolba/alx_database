00d85ea616ea991a1e9cb564633831d971b9cb6c
DinaTolba <magdydina2002@gmail.com>
2024-03-12 23:50:06 +0200

    task3

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,4 @@
---script that lists all the tables of a specified database on your MySQL server.
-USE dbname;
+-- Script that lists all the tables of a specified database on your MySQL server.
+
+-- Show tables in the specified database
 SHOW TABLES;
